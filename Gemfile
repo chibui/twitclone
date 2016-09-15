df811e049b961d9365798978cbc8e9fd83748c50
chibui <chi_bui@outlook.com>
2016-09-15 16:02:59 +1000

    add tweet scaffold

--- Gemfile
+++ Gemfile
@@ -13,8 +13,11 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+# Use font-awesome-sass
 gem 'font-awesome-sass', '~> 4.6.2'
+# Use bootstrap-sass
 gem 'bootstrap-sass', '~> 3.3.6'
+# Use devise
 gem 'devise'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915040806) do
+ActiveRecord::Schema.define(version: 20160915055358) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "User_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tweets", ["User_id"], name: "index_tweets_on_User_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
